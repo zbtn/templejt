00b25a830b8192fa7431d6ada8051fbc2bfddfbb
zbtn <zbytnievzky@gmail.com>
2016-11-30 08:35:55 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        watch: {
+            files: 'app/style/**/*.scss',
+            tasks: ['sass']
+        },
+        sass: {
+            dev: {
+                files: {
+                    'app/public/css/main.css': 'app/style/main.scss'
+                }
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'app/public/css/*.css',
+                        'app/public/*.js',
+                        'app/public/*.html',
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './app/public'
+                }
+            }
+        }
+    });
+
+    // load npm tasks
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    // define default task
+    grunt.registerTask('default', ['browserSync', 'watch']);
+};
